abeff8ad32248bc73fcc1ff3e27c162496ca558e
Lady3mlnm <lady3mlnm@gmail.com>
2018-10-26 22:20:59 +0300

    Step 7. Some improvements

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -2,9 +2,12 @@ package com.rtjvm.scala.oop.commands
 
 import com.rtjvm.scala.oop.filesystem.State
 
-trait Command {
-  def apply(state: State): State
+trait Command extends (State => State) {
+
 }
+//trait Command {
+//  def apply(state: State): State
+//}
 
 object Command {
   val INFO = "info"
@@ -31,31 +34,33 @@ object Command {
 
     if (input.isEmpty || tokens.isEmpty)
       emptyCommand
-    else if (INFO.equals(tokens(0))) {
-      new Info
-    } else if (MKDIR.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(MKDIR)
-      else new Mkdir(tokens(1))
-    } else if (LS.equals(tokens(0))) {
-      new Ls
-    } else if (PWD.equals(tokens(0))) {
-      new Pwd
-    } else if (TOUCH.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(TOUCH)
-      else new Touch(tokens(1))
-    } else if (CD.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CD)
-      else new Cd(tokens(1))
-    } else if (RM.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(RM)
-      else new Rm(tokens(1))
-    } else if (ECHO.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(ECHO)
-      else new Echo(tokens.tail)
-    } else if (CAT.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CAT)
-      else new Cat(tokens(1))
-    } else
-      new UnknownCommand
+    else tokens(0) match {
+      case INFO =>
+        new Info
+      case MKDIR =>
+        if (tokens.length < 2) incompleteCommand(MKDIR)
+        else new Mkdir(tokens(1))
+      case LS =>
+        new Ls
+      case PWD =>
+        new Pwd
+      case TOUCH =>
+        if (tokens.length < 2) incompleteCommand(TOUCH)
+        else new Touch(tokens(1))
+      case CD =>
+        if (tokens.length < 2) incompleteCommand(CD)
+        else new Cd(tokens(1))
+      case RM =>
+        if (tokens.length < 2) incompleteCommand(RM)
+        else new Rm(tokens(1))
+      case ECHO =>
+        if (tokens.length < 2) incompleteCommand(ECHO)
+        else new Echo(tokens.tail)
+      case CAT =>
+        if (tokens.length < 2) incompleteCommand(CAT)
+        else new Cat(tokens(1))
+      case _ =>
+        new UnknownCommand
+    }
   }
 }
--- src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
+++ src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -1,17 +1,33 @@
 package com.rtjvm.scala.oop.filesystem
 
-import java.util.Scanner
 import com.rtjvm.scala.oop.commands.Command
 import com.rtjvm.scala.oop.files.Directory
 
 object Filesystem extends App {
   val root: Directory = Directory.ROOT
   var state: State = State(root, root)
-  val scanner: Scanner = new Scanner(System.in)
 
-  while(true) {
-    state.show
-    val input = scanner.nextLine()
-    state = Command.from(input).apply(state)
-  }
+  /*
+    0 (op) 1 => 1
+    1 (op) 2 => 3
+    3 (op) 3 => 6
+    6 (op) 4 => your last value, 10
+
+    List(1,2,3,4).foldLeft(0)((x, y) => x + y)
+   */
+  state.show
+  io.Source.stdin.getLines().foldLeft(state)((currentState, newLine) => {
+        state = Command.from(newLine).apply(state)
+        state.show
+        state
+  })
+
+  //var state: State = State(root, root)
+  //val scanner: Scanner = new Scanner(System.in)
+  //
+  //while(true) {
+  //  state.show
+  //  val input = scanner.nextLine()
+  //  state = Command.from(input).apply(state)
+  //}
 }
